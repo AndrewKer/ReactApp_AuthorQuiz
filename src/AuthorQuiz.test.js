c14f7bab87dbbf5d720775a228267578ba50c67d
Andrew <andrew.kerlegand@ge.com>
2018-08-04 13:33:24 -0500

    Added enzyme library for testing purposes. Added a default state to pass test#1. Created a second test for when no answer has been selected.

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+//npm install enzyme enzyme-adapter
+import Enzyme,{mount,shallow,render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter() });
+
+const state = {
+  turnData:{
+    books:['book1','book2', 'book3', 'book4', 'book5'],
+    author:{
+      name: 'Author 1',
+      imageUrl: 'images/authors/author1.jpg',
+      imageSource: '',
+      books: ['book1']
+    }
+  },
+  highlight:'none'
+}
 
 describe("Author Quiz", ()=>{
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<AuthorQuiz />, div);
-    //ReactDOM.unmountComponentAtNode(div);
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />, div);
+  });
+
+  describe("When no answer has been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />);
+    });
+
+    it("should have no background color", ()=>{
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
   });
 });
+
+
 
